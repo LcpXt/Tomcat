a4d1136447f634faf24b1b0f2aea389936f14041
lcp <1952950999@qq.com>
2024-06-26 16:39:35 +0800

    模拟Tomcat使用进行测试

--- src/com/colin/user/servlet/ShowRequestServlet.java
+++ src/com/colin/user/servlet/ShowRequestServlet.java
@@ -1,5 +1,6 @@
 package com.colin.user.servlet;
 
+import com.colin.servlet.Cookie;
 import com.colin.servlet.HttpServlet;
 import com.colin.servlet.HttpServletRequest;
 import com.colin.servlet.HttpServletResponse;
@@ -16,8 +17,21 @@ import java.io.PrintWriter;
 public class ShowRequestServlet extends HttpServlet {
     @Override
     public void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        User user = new User(1, "aaa", "bbb");
-        req.setAttribute("user", user);
-        req.getRequestDispatcher("/target").forward(req, resp);
+//        Cookie[] cookies = req.getCookies();
+//        for (Cookie cookie : cookies) {
+//            System.out.println(cookie.getKey() + ":" + cookie.getValue());
+//        }
+//        resp.addCookie(new Cookie("acs", "sssss"));
+        Cookie cookie1 = new Cookie("sql", "111");
+        cookie1.setMaxAge(360000);
+        cookie1.setPath("/login");
+        cookie1.setHttpOnly(true);
+        resp.addCookie(cookie1);
+
+        Cookie cookie2 = new Cookie("slss", "dwd");
+        cookie2.setMaxAge(360000);
+//        cookie2.setPath("/login");
+        cookie2.setHttpOnly(false);
+        resp.addCookie(cookie2);
     }
 }
